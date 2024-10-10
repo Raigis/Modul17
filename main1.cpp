5b07c9f708da55a205970e842a317c3e2dc91acd
Александр <146124268+Raigis@users.noreply.github.com>
2024-10-10 11:43:52 +0000

    Complete First Task

--- main1.cpp
+++ main1.cpp
@@ -1,1 +1,18 @@
+/*
+Напишите функцию, принимающую два указателя на int и меняющую местами содержимое этих указателей.
+*/
 
+#include <iostream>
+
+void swap (int* pa, int* pb) {
+    int temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+}
+
+int main () {
+    int a = 10;
+    int b = 20;
+    swap(&a, &b);
+    std::cout << a << " " << b;
+}
